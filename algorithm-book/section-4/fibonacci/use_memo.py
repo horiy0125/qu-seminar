defd7d7755e4b54627e6016c30f2d1eac6307fe5
horri1520 <kh1520.shsf@gmail.com>
2021-04-30 00:05:08 +0900

    feat: implement use_memo fibonacci function

---
+++ algorithm-book/section-4/fibonacci/use_memo.py
@@ -0,0 +1,27 @@
+memo = []
+
+
+def use_memo(N: int) -> int:
+
+    global memo
+
+    if len(memo) == 0:
+        for _ in range(N+1):
+            memo.append(-1)
+
+    if N == 0 or N == 1:
+        fibonacci_number = memo[N]
+        if fibonacci_number == -1:
+            fibonacci_number = N
+            memo[N] = fibonacci_number
+
+    else:
+        fibonacci_number = memo[N]
+        if fibonacci_number == -1:
+            fibonacci_number = use_memo(N-1) + use_memo(N-2)
+            memo[N] = fibonacci_number
+
+    return fibonacci_number
+
+
+print(use_memo(24))
