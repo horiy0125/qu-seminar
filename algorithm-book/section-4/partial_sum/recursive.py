fd845e4a102f09bb2f940e94974ea52cf17b272f
horri1520 <kh1520.shsf@gmail.com>
2021-05-07 12:55:12 +0900

    feat: implement recursive partial sum function

---
+++ algorithm-book/section-4/partial_sum/recursive.py
@@ -0,0 +1,22 @@
+def recursive(n: int, numbers: list, target_sum: int):
+    print(n, target_sum, numbers)
+    if n == 0:
+        return target_sum == 0
+    else:
+        if sum(numbers) == target_sum:
+            return True
+        else:
+            results = [
+                recursive(n-1, numbers, target_sum),
+                recursive(n-1, numbers, target_sum - numbers[n-1])
+            ]
+
+            return True in results
+
+
+N = 4
+a = [3, 2, 6, 5]
+W = 14
+
+possible = recursive(N, a, W)
+print(possible)
