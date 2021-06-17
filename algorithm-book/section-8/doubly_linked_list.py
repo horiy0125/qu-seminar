2a227ac17cb96e79dad88f9c31a4b0a06c3fd372
horri1520 <kh1520.shsf@gmail.com>
2021-06-17 22:38:46 +0900

    feat: complete to implement doubly_linked_list

--- algorithm-book/section-8/doubly_linked_list.py
+++ algorithm-book/section-8/doubly_linked_list.py
@@ -1,4 +1,4 @@
-class Element:
+class Cell:
     def __init__(self, x) -> None:
         self.x = x
         self.next = None
@@ -9,8 +9,8 @@ class Doubly_linked_list:
     def __init__(self) -> None:
         self.head = None
 
-    def insert_tail(self, x) -> None:
-        new = Element(x)
+    def append(self, x) -> None:
+        new = Cell(x)
 
         # 1つ目の要素を追加した場合
         if self.head == None:
@@ -23,20 +23,133 @@ class Doubly_linked_list:
         new.next = self.head
         self.head.prev = new
 
-    def show(self) -> None:
-        tmp_element = self.head
-        while tmp_element:
-            print(tmp_element.x, 'prev:', tmp_element.prev.x,
-                  'next:', tmp_element.next.x)
-            tmp_element = tmp_element.next
+    def insert_head(self, x) -> None:
+        new = Cell(x)
 
-            if tmp_element == self.head:
+        # 1つ目の要素を追加した場合
+        if self.head == None:
+            self.head = new
+            self.head.next = new
+            self.head.prev = new
+
+        new.next = self.head
+        new.prev = self.head.prev
+        self.head.prev.next = new
+        self.head.prev = new
+        self.head = new
+
+    def delete(self, x) -> None:
+        tmp_cell = self.head
+        head_cell = self.head
+
+        if self.head == None:
+            raise ValueError('delete from empty list')
+
+        delete_count = 0
+        while tmp_cell:
+            if tmp_cell.x == x:
+                delete_count += 1
+                tmp_cell.prev.next = tmp_cell.next
+                tmp_cell.next.prev = tmp_cell.prev
+                if tmp_cell == self.head:
+                    self.head = self.head.next
                 return
+            tmp_cell = tmp_cell.next
+            if tmp_cell == head_cell:
+                if delete_count == 0:
+                    raise ValueError('value not in list')
+                return
+
+    def delete_head(self) -> None:
+        # O(1)
+        if self.head == None:
+            raise ValueError('delete from empty list')
+
+        # 要素が1つしかない場合
+        if self.head.next == self.head:
+            self.head = None
+            return
+
+        self.head.prev.next = self.head.next
+        self.head.next.prev = self.head.prev
+        self.head = self.head.next
+
+    def delete_tail(self) -> None:
+        # O(1)
+        if self.head == None:
+            raise ValueError('delete from empty list')
+
+        # 要素が1つしかない場合
+        if self.head.next == self.head:
+            self.head = None
+            return
+
+        self.head.prev.prev.next = self.head
+        self.head.prev = self.head.prev.prev
+
+    def std_list(self) -> list:
+        # O(N)
+        l = []
+
+        tmp_cell = self.head
+
+        while tmp_cell:
+            l.append(tmp_cell.x)
+            tmp_cell = tmp_cell.next
+            if tmp_cell == self.head:
+                break
+
+        return l
+
+    # def show(self) -> None:
+    #     tmp_cell = self.head
+    #     print('[', end='')
+
+    #     while tmp_cell:
+    #         print(f"{tmp_cell.x}", end='')
+    #         tmp_cell = tmp_cell.next
+
+    #         if tmp_cell == self.head:
+    #             print(']')
+    #             return
+    #         else:
+    #             print(', ', end='')
 
 
 l = Doubly_linked_list()
-l.insert_tail(0)
-l.insert_tail(1)
-l.insert_tail(2)
-l.insert_tail(3)
-l.show()
+print(l.std_list())
+print()
+
+print('add 1, 2, 3 (int) to the list')
+l.append(1)
+l.append(2)
+l.append(3)
+print(l.std_list())
+print()
+
+print('add 0 to head of the list')
+l.insert_head(0)
+print(l.std_list())
+print()
+
+print('add 4, 5, 6 (int) and hoge (str) to the list')
+l.append(4)
+l.append(5)
+l.append(6)
+l.append('hoge')
+print(l.std_list())
+print()
+
+print('delete 6 from the list')
+l.delete(6)
+print(l.std_list())
+print()
+
+print('delete last cell (str hoge) from list')
+l.delete_tail()
+print(l.std_list())
+print()
+
+print('delete head cell (int 0) from the list')
+l.delete_head()
+print(l.std_list())
