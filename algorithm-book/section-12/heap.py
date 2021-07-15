782901ac9999419ab8483a309b4377f7c0943ea3
horri1520 <kh1520.shsf@gmail.com>
2021-07-16 01:48:31 +0900

    feat: implement pop method in Heap class

--- algorithm-book/section-12/heap.py
+++ algorithm-book/section-12/heap.py
@@ -6,9 +6,13 @@ class Heap:
         if len(self.heap) > 0:
             return self.heap[0]
         else:
-            raise IndexError('heap has no value')
+            return
+            # raise IndexError('length of heap is 0')
 
     def push(self, value) -> None:
+        if type(value) != int and type(value) != float:
+            raise TypeError('value must be number')
+
         self.heap.append(value)
         i = len(self.heap) - 1
 
@@ -22,6 +26,30 @@ class Heap:
             i = p
 
         self.heap[i] = value
+
+    def pop(self) -> None:
+        if len(self.heap) == 0:
+            return
+
+        l = len(self.heap) - 1
+        top = self.heap[l]
+        self.heap = self.heap[:l]
+
+        i = 0
+        while i * 2 + 1 < len(self.heap):
+            child1 = i * 2 + 1
+            child2 = i * 2 + 2
+
+            if child2 < len(self.heap) and self.heap[child2] > self.heap[child1]:
+                child1 = child2
+
+            if self.heap[child1] <= top:
+                break
+
+            self.heap[i] = self.heap[child1]
+            i = child1
+
+        self.heap[i] = top
 
 
 heap = Heap()
@@ -37,4 +65,6 @@ heap.push(7)
 heap.push(5)
 heap.push(3)
 heap.push(2)
+print(heap.heap, len(heap.heap))
+heap.pop()
 print(heap.heap, len(heap.heap))
